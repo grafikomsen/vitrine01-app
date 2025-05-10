196e38a3c0872a1048fc7756359013ff9e8b7fc2
Grafikomsen <grafikomsen@gmail.com>
2025-05-10 16:58:30 +0000

    Modification de slug et le SEO1 commit

--- database/migrations/2025_05_10_125008_alter_blogs_table.php
+++ database/migrations/2025_05_10_125008_alter_blogs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('blogs', function(Blueprint $table){
-            $table->string('slug')->unique()->after('name');
+            $table->string('slug')->after('name');
         });
     }
 
